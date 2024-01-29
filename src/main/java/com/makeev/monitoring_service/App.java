59fe3cd766e0ef32a562e97b0be2dcd70a78f885
Evgeniy Makeev <evgeniyvmakeev@gmail.com>
2024-01-29 10:56:35 +0300

    for PR

--- src/main/java/com/makeev/monitoring_service/App.java
+++ src/main/java/com/makeev/monitoring_service/App.java
@@ -1,20 +1,6 @@
 package com.makeev.monitoring_service;
 
-import com.makeev.monitoring_service.ui.ApplicationContext;
-
-/**
- * The main class representing the utility meter reading application.
- * The application allows users to manage their utility meter readings, log in, and perform various actions.
- * @author Evgeniy Makeev
- * @version 1.4
- */
 public class App {
-    /**
-     * The main method to start the application.
-     *
-     * @param args The command-line arguments.
-     */
     public static void main(String[] args) {
-        new ApplicationContext().start();
     }
 }
